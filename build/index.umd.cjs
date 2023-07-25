13651aa7b9c36a821335f467992a5f048a6cf0cd
Kirill Evdokimov <kirill@diagnocat.com>
2023-07-25 19:42:55 +0400

    Bring Reference section back to undex.umd.cjs

--- build/index.umd.cjs
+++ build/index.umd.cjs
@@ -788,6 +788,48 @@
         return true;
     }
 
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////   Reference
+    /////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Subdivision Surfaces
+    //      https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/thesis-10.pdf
+    //      https://en.wikipedia.org/wiki/Loop_subdivision_surface
+    //      https://cseweb.ucsd.edu/~alchern/teaching/cse167_fa21/6-3Surfaces.pdf
+    //
+    // Original three.js SubdivisionModifier, r124 (Loop)
+    //      https://github.com/mrdoob/three.js/blob/r124/examples/jsm/modifiers/SubdivisionModifier.js
+    //
+    // Original three.js SubdivisionModifier, r59 (Catmull-Clark)
+    //      https://github.com/mrdoob/three.js/blob/r59/examples/js/modifiers/SubdivisionModifier.js
+    //
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////   License
+    /////////////////////////////////////////////////////////////////////////////////////
+    //
+    // MIT License
+    //
+    // Copyright (c) 2022 Stephens Nunnally <@stevinz>
+    //
+    // Permission is hereby granted, free of charge, to any person obtaining a copy
+    // of this software and associated documentation files (the "Software"), to deal
+    // in the Software without restriction, including without limitation the rights
+    // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    // copies of the Software, and to permit persons to whom the Software is
+    // furnished to do so, subject to the following conditions:
+    //
+    // The above copyright notice and this permission notice shall be included in all
+    // copies or substantial portions of the Software.
+    //
+    // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    // SOFTWARE.
+
+
     exports.LoopSubdivision = LoopSubdivision;
 
     Object.defineProperty(exports, '__esModule', { value: true });
